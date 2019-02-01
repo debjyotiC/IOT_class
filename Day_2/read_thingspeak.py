e0a3ee12f5142de3d02586b7e65962b87be897a8
Debjyoti Chowdhury <djbabai.debjyoti@gmail.com>
2019-02-01 22:34:29 +0530

    added code to read Thingspeak data

---
+++ Day_2/read_thingspeak.py
@@ -0,0 +1,28 @@
+import urllib2
+import json
+import time
+
+READ_API_KEY = ''
+CHANNEL_ID =
+
+
+# get data from cloud
+def get_data():
+    connt_speak = urllib2.urlopen(
+        "http://api.thingspeak.com/channels/%s/feeds/last.json?api_key=%s" % (CHANNEL_ID, READ_API_KEY))
+    response = connt_speak.read()
+    data = json.loads(response)
+    temp = data['field1']
+    updated_at = data['created_at']
+    data_str = "The sensor val:" + temp + " Last updated at:" + updated_at
+    print data_str
+
+
+def main():
+    while True:
+        get_data()
+        time.sleep(10)
+
+
+if __name__ == '__main__':
+    main()
